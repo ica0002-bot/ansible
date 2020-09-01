17c07b3fad9960b833f3a4c49b73eecef95aa7a6
Juri Hudolejev <jhudolejev@gmail.com>
2020-09-01 20:05:30 +0300

    Clean host SSH keys

--- roles/vm_admin/files/student-vms.sh
+++ roles/vm_admin/files/student-vms.sh
@@ -8,9 +8,6 @@ fi
 vm_count="$1"
 
 for student in $(cat "/opt/ica0002/data/students-with-github-set-up.txt" | grep '.'); do
-    if [ "$student" = "romankuchin" ]; then
-        continue;
-    fi
     echo "Provisioning $vm_count VMs for $student..."
     /usr/local/bin/vm-admin "$student" "$vm_count"
 done
