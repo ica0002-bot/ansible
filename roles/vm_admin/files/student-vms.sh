7b0220365f998b2952991a42186dd3dc699f58a2
Juri Hudolejev <jhudolejev@gmail.com>
2020-09-01 12:22:57 +0300

    Add student-vms script

---
+++ roles/vm_admin/files/student-vms.sh
@@ -0,0 +1,13 @@
+#!/bin/sh -eu
+
+if [ "$#" -lt 1 ]; then
+    echo "Usage: $(basename $0) {0|1|2|3}"
+    exit 1
+fi
+
+vm_count="$1"
+
+for student in $(cat "/opt/ica0002/data/students-with-github-set-up.txt" | grep '.'); do
+    echo "Provisioning $vm_count VMs for $student..."
+    /usr/local/bin/vm-admin "$student" "$vm_count"
+done
